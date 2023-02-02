8d0985d67d243350742e8730a67a0255d7bfcddf
Nguyen Huy Dat <datnh10>
2023-02-02 13:34:42 +0700

    add

--- index_eth.js
+++ index_eth.js
@@ -209,7 +209,7 @@ const updatePriceForSell =async (coinName2,timeRequest, so_nen_check_giao_cat)=>
 						total_coin_phanky+=1
 
 					//	bot.sendMessage(chatId, total_coin_phanky + "  " + timeRequest+  ", phan ki ban " + coinName2 +"  "+ intersect_macd_index_array[i]+"   " + lastPrice);
-						logStr += total_coin_phanky+ "  "+  timeRequest +", phan ki giam " + coinName2 +"  "+ intersect_macd_index_array[i]+"   " + lastPrice +"\n"
+						logStr += total_coin_phanky+ "  "+  timeRequest +", phan ki giam \n" + coinName2 +"  "+ intersect_macd_index_array[i]+"   " + lastPrice +"\n"
 					
 						console.log( logStr)
 						//	bot.sendMessage(chatId,logStr );
@@ -424,7 +424,7 @@ const updatePriceForBuy =async (coinName2,timeRequest)=>{
                             //   console.log("Ema10 " + (ema10))
 
                                 coinDivergenceList.push(coinName2)
-                                logStr += total_coin_phanky+ "  "+  timeRequest +", phan ki tang " + coinName2 +"  "+ intersect_macd_index_array[i]+"   " + lastPrice +"\n"
+                                logStr += total_coin_phanky+ "  "+  timeRequest +", phan ki tang \n " + coinName2 +"  "+ intersect_macd_index_array[i]+"   " + lastPrice +"\n"
                                 console.log( logStr)
                              //   bot.sendMessage(chatId,logStr );
 								// if((timeRequest == "5m") || (timeRequest == "15m") ){
@@ -521,7 +521,7 @@ const updatePrice = async(timeRequest )=>{
 					  if((test15m.hasPhanKy > 1) && (test5m.hasPhanKy > 1))
 					  {
 						console.log("test5m phan ky buy " +test5m.hasPhanKy+ "   logData2  : "+ test5m.logStr)
-						var logData = "Phan ky 2 lan : "+  test5m.logStr + test15m.logStr + test30m.logStr + test1h.logStr;
+						var logData = "Phan ky 2 lan : \n "+  test5m.logStr + test15m.logStr + test30m.logStr + test1h.logStr;
 						bot.sendMessage(chatId,logData );
 					  }
 					  // check for shell
@@ -768,7 +768,7 @@ const waitSellOrderCompletion = async()=>{
 			// 	console.log("Doi mua");
 			// }
 			// if(buySuccess === 'failure') continue; 
-			bot.sendMessage(chatId," =============Ket thuc 1 vong requets ======" );
+		//	bot.sendMessage(chatId," =============Ket thuc 1 vong requets ======" );
 		await wait(10000);
 	}
 
