85d387ee3c719ac77274b7db9b960b013693d493
Nguyen Huy Dat <datnh10>
2023-02-02 11:55:33 +0700

    add

--- index_eth.js
+++ index_eth.js
@@ -768,7 +768,7 @@ const waitSellOrderCompletion = async()=>{
 			// 	console.log("Doi mua");
 			// }
 			// if(buySuccess === 'failure') continue; 
-		//	bot.sendMessage(chatId," =============Ket thuc 1 vong requets ======" );
+			bot.sendMessage(chatId," =============Ket thuc 1 vong requets ======" );
 		await wait(10000);
 	}
 
